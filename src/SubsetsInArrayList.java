c8f9058dbc3a18ec938f561902ed5dc43f2a7690
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-09-18 00:12:30 +0530

    Returning sets in arraylist

--- src/SubsetsInArrayList.java
+++ src/SubsetsInArrayList.java
@@ -2,27 +2,25 @@ import java.sql.Array;
 import java.util.ArrayList;
 
 public class SubsetsInArrayList {
+
     public static void main(String[] args) {
-    subseq("", "adbsdfsa");
+      ArrayList<String> result =   sets("", "abc");
+        System.out.println(result);
     }
 
-    static ArrayList<String> subseq(String p, String up){
-    if(up.isEmpty()){
-        ArrayList<String> list = new ArrayList<>();
-        list.add(p);
-        return list;
+    static ArrayList<String> sets(String p, String up){
+        if(up.isEmpty()){
+            ArrayList <String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+
+        ArrayList<String> left = sets(p+ch, up.substring(1));
+        ArrayList<String> right = sets(p, up.substring(1));
+        left.addAll(right);
+        return left;
     }
-
-    char ch = up.charAt(0);
-
-    ArrayList <String> left = subseq(p+ch, up.substring(1));
-    ArrayList <String> right = subseq(p, up.substring(1));
-
-    left.addAll(right);
-
-    return left;
-
-    }
-
 
 }
