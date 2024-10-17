fc2a2548a2eff7443fc37208f3a1e991765d193d
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-10-17 11:24:54 +0530

    insert at first and display in LL

---
+++ src/LinkedList/SinglyLL/LL.java
@@ -0,0 +1,50 @@
+package LinkedList.SinglyLL;
+
+public class LL {
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public void insertAtFirst(int val){
+        Node node = new Node(val);
+        node.next=head;
+        head=node;
+
+        if(tail==null){
+            tail=head;
+        }
+
+        size += 1;
+    }
+
+    public void display(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.value + "->");
+            temp=temp.next;
+        }
+        System.out.println("END");
+    }
+
+
+    public LL(){
+        this.size=0;
+    }
+
+    private class Node{
+        private int value;
+        private Node next;
+
+
+        public Node(int value){
+            this.value = value;
+        }
+
+        public Node(int value, Node next){
+            this.value=value;
+            this.next=next;
+        }
+
+    }
+}
---
+++ src/LinkedList/SinglyLL/Main.java
@@ -0,0 +1,13 @@
+package LinkedList.SinglyLL;
+
+public class Main {
+    public static void main(String[] args) {
+     LL list = new LL();
+
+        list.insertAtFirst(3);
+        list.insertAtFirst(2);
+        list.insertAtFirst(8);
+        list.insertAtFirst(17);
+        list.display();
+    }
+}
