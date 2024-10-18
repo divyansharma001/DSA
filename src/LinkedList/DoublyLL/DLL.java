d8e104f4bb38eeef5720fe7c2e00cabd14b6ce38
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-10-18 11:29:00 +0530

    reversal of Linked List

--- src/LinkedList/DoublyLL/DLL.java
+++ src/LinkedList/DoublyLL/DLL.java
@@ -1,6 +1,48 @@
 package LinkedList.DoublyLL;
 
 public class DLL {
+
+    private Node head;
+
+    public void insertAtFirst(int val){
+        Node node = new Node(val);
+        node.next = head;
+        node.prev= null;
+        if(head!=null){
+            head.prev=node;
+        }
+        head=node;
+    }
+
+//    public void display(){
+//        Node node = head;
+//        while(node!=null){
+//            System.out.print(node.val + "->");
+//            node = node.next;
+//        }
+//
+//        System.out.println("END");
+//
+//    }
+
+    public void display(){
+        Node node = head;
+        Node last = null;
+        while(node!=null){
+            System.out.print(node.val + "->");
+            last = node;
+            node=node.next;
+        }
+        System.out.println("END");
+
+        System.out.println("PRINT IN REVERSE");
+        while(last!=null){
+            System.out.print(last.val + "->");
+            last = last.prev;
+        }
+        System.out.println("START");
+    }
+
 public class Node{
     int val;
     Node next;
---
+++ src/LinkedList/DoublyLL/Main.java
@@ -0,0 +1,12 @@
+package LinkedList.DoublyLL;
+
+public class Main {
+    public static void main(String[] args) {
+        DLL list = new DLL();
+        list.insertAtFirst(3);
+        list.insertAtFirst(4);
+        list.insertAtFirst(5);
+        list.insertAtFirst(6);
+        list.display();
+    }
+}
