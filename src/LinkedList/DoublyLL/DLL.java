decbac04da6cc12a84931b8537694f4fad62a939
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-10-18 10:48:38 +0530

    Doubly LL Insertion and display

--- src/LinkedList/DoublyLL/DLL.java
+++ src/LinkedList/DoublyLL/DLL.java
@@ -1,5 +1,19 @@
 package LinkedList.DoublyLL;
 
 public class DLL {
+public class Node{
+    int val;
+    Node next;
+    Node prev;
 
+    public Node(int val){
+        this.val = val;
+    }
+
+    public Node(int val, Node next, Node prev){
+        this.val = val;
+        this.next = next;
+        this.prev = prev;
+    }
+}
 }
